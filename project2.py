ee7d01c8838ef9c9beb76d98fea48a0c406cb0cb
Ovanjohn <ovanjohn@yahoo.com>
2023-11-25 11:57:05 +0100

    learningpy

--- project2.py
+++ project2.py
@@ -9,3 +9,20 @@ for attempt in range(max_attempts):
     break
   else:
     print("Incorrect Username")
+
+#Creating a user login system using While Loop
+Max_attempts = 3
+correct_username = "admin"
+correct_password = "12345"
+attempts = 1
+username = input("Enter your username")
+password = input("Enter your password")
+while attempts < Max_attempts:
+  if(username == correct_username and password == correct_password):
+   break
+  else:
+      print("incorrect username or password")
+      username = input("Enter your username")
+      password = input("Enter your password")
+      attempts = attempts + 1
+      print(f"your number of attempts is { attempts}")
